0b6e0e8c5104bd22d4e5a771c47122cf4f898ef3
ninebelowzero <richardjohnsilk@gmail.com>
2016-10-29 22:27:22 +0100

    Changed function structure.

--- js/app.js
+++ js/app.js
@@ -5,26 +5,41 @@ function MazeController() {
 
     var maze = this;
 
-    var gridHeight = 10;
-    var gridWidth = 10;
+    var gridHeight  = 10,
+        gridWidth   = 10,
+        interval    = 500;
 
-    reset(gridHeight, gridWidth);
+
+    maze.reset = reset;
+
+    maze.reset();
 
 
-    function reset(gridHeight, gridWidth) {
+    // Functions (hoisted)
 
-        maze.grid = [];
+    function reset () {
+        maze.grid = buildGrid(gridHeight, gridWidth);
+        findPath();
+    }
+
+    function buildGrid(gridHeight, gridWidth) {
+
+        var grid = [];
 
         for (var i = 0; i < gridHeight; i++) {
 
             var row = [];
-
             for (var j = 0; j < gridWidth; j++) {
-                row.push({ open: true, right: false, below: false });
+                row.push({ open: false, right: false, below: false });
             }
-
-            maze.grid.push(row);
+            grid.push(row);
         }
+
+        return grid;
+    }
+
+    function findPath() {
+
     }
 
 }
